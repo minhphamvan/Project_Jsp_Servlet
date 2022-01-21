68b35367b5f4cf87f899ca0cdc8a6c36f55a76b8
minhpham <mnhphm136@gmail.com>
2022-01-21 18:31:04 +0700

    ok

--- Online_Store_Jsp_Servlet/src/main/java/com/online_store/controller/client/IndexController.java
+++ Online_Store_Jsp_Servlet/src/main/java/com/online_store/controller/client/IndexController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher rd = req.getRequestDispatcher("/client/index.jsp");
+		RequestDispatcher rd = req.getRequestDispatcher("/client/view/index.jsp");
 		rd.forward(req, resp);
 	}
 }
